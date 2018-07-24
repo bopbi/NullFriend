5f9b9bba9d77baad0d21f2e42842899e51b4581e
Bobby Prabowo <prabowo.bobby@gmail.com>
2018-07-24 23:11:59 +0700

    add supress

--- src/main/kotlin/NullFriend.kt
+++ src/main/kotlin/NullFriend.kt
@@ -9,7 +9,7 @@ fun <T> isNull(elements: List<T>) : Boolean {
     return false
 }
 
-
+@Suppress("UNCHECKED_CAST")
 inline fun <T> whenItsNotNull(elements: List<T?>, action: (List<T>) -> Unit) {
     if (!isNull(elements)) {
         action(elements as List<T>)
